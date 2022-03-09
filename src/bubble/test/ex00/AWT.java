b7344be21f257793ac2c3424b0a75657cb3ffbc3
Rhymezee <itsthehiphop@naver.com>
2022-03-09 18:59:18 +0900

    이미지 및 BGM 가져오기

---
+++ src/bubble/test/ex00/AWT.java
@@ -0,0 +1,17 @@
+package bubble.test.ex00;
+
+public class AWT {
+	
+	/*
+	 * AWT 라이브러리
+	 * 
+	 * 1. 그림 그리는 라이브러리
+	 * 2. 윈도우 창을 만들 수 있다. (JFrame)
+	 * 3. 윈도우 창(JFrame) 안에 도화지(JPanel)가 내장되어 있다.
+	 * 4. JFrame은 while이 돌고 있어 계속 켜져 있다.
+	 * 5. AWT의 Component에는 버튼, 이미지뷰, 체크박스, 셀렉트박스 등 이런 것들이 만들어져 있다.
+	 * 6. Component는 위젯이라고 생각하면 편하다. -> 재사용
+	 * 
+	 */
+
+}
