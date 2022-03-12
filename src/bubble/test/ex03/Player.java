de11cf363ba5d16ecfa3faf45668d98455e79660
Rhymezee <itsthehiphop@naver.com>
2022-03-12 18:04:57 +0900

    이벤트 리스너 원리

--- src/bubble/test/ex03/Player.java
+++ src/bubble/test/ex03/Player.java
@@ -52,6 +52,15 @@ public class Player extends JLabel implements Moveable {
 
 	@Override
 	public void right() {
+		/*
+		 * Thread.sleep(2000); = 2초 멈췄다가 이동시킴.
+		 * 예를 들어 오른쪽 버튼을 5번 누르면 10초 후에 오른쪽으로 5칸 순간이동 한다.
+		 * 그림 변경 시점은 이벤트 루프의 모든 임무(task)가 완료되어야 repaint가 된다.
+		 * 
+		 * 메인 스레드는 key를 이벤트 루프에게 전달한다.
+		 * 메인 스레드만 있으면 동시에 2개의 key를 누르면 하나의 key만 전달할 수 있다.
+		 */
+		
 		setIcon(playerR);
 		x = x + 10;
 		setLocation(x, y);
